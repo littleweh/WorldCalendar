77bcd4e7117b74f13db1a608a894c34ffae4bd01
aidakao <adakao@kkbox.com>
2022-03-27 13:59:16 +0800

    add WorldCalendar project

---
+++ WorldCalendar/WorldCalendarApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorldCalendarApp.swift
+//  WorldCalendar
+//
+//  Created by 小威 on 2022/3/27.
+//
+
+import SwiftUI
+
+@main
+struct WorldCalendarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
